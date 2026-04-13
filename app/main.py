63b26bcd7a0bdf64eb610f2dc10e4e7640ca3b09
buldiga_programmer1337 <buldin-zakhar@mail.ru>
2026-04-13 09:38:11 +0300

    frontend+backend

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(users.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # или ["*"] для всех (только для разработки)
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5177", "http://127.0.0.1:5177"],  # или ["*"] для всех (только для разработки)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
